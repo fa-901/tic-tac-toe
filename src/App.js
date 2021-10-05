d2893472331e39146658ef7c9dddbda1072d4d31
Farhan Alam <metalhead901@live.com>
2021-10-06 01:15:41 +0600

    added horizontal and vertical rule checking for game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './styles/style.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import { getStatus, getCurrent, tileClick, restart } from './redux/slices/gameSlice';
+import { getBoardStatus, tileClick, restart } from './redux/slices/gameSlice';
 import GridItem from './components/GridItem';
 
 
 function App() {
 	const dispatch = useDispatch();
-	const boardStatus = useSelector(getStatus);
-	const tiles = boardStatus.map((row, rowIndex) => {
+	const boardStatus = useSelector(getBoardStatus);
+	const tiles = boardStatus.gridState.map((row, rowIndex) => {
 		return row.map((col, colIndex) => {
 			return (
 				<GridItem
@@ -18,10 +18,28 @@ function App() {
 			)
 		})
 	})
+
+	const historyList = boardStatus.history.map((item, index)=>{
+		return <li key={index}>{item}</li>
+	})
+
 	return (
 		<div className='container mx-auto h-screen text-center py-20 flex flex-col justify-center items-center'>
-			<div className="tictac-main grid grid-cols-3">
-				{tiles}
+			<div className='text-3xl mb-10'>
+				Player Turn: <b>{boardStatus.currentTurn}</b>
+			</div>
+			<div className="grid grid-cols-2 space-x-10">
+				<div className="tictac-main grid grid-cols-3 flex-1">
+					{tiles}
+				</div>
+				<div className=''>
+					{/* <u>
+						Moves
+					</u> */}
+					<ul className='text-left list-disc list-inside'>
+						{historyList}
+					</ul>
+				</div>
 			</div>
 			<button className='btn-restart' onClick={() => { dispatch(restart()) }}>Restart</button>
 		</div>
--- src/redux/slices/gameSlice.js
+++ src/redux/slices/gameSlice.js
@@ -1,12 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const defaultState = {
+    gridState: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ],
+    history: [],
+    winner: '',
     currentTurn: 'X',
-    boardStatus: [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
-    ]
 }
 
 export const gameSlice = createSlice({
@@ -14,9 +16,11 @@ export const gameSlice = createSlice({
     initialState: defaultState,
     reducers: {
         tileClick: (state, action) => {
-            const { currentTurn } = state;
+            const { currentTurn, gridState, history } = state;
             const [row, col] = action.payload;
-            state.boardStatus[row][col] = currentTurn;
+            state.gridState[row][col] = currentTurn;
+            state.history.push(`${currentTurn} on row ${row + 1}, column ${col + 1}`);
+            state.winner = getWinner(gridState, currentTurn, history);
             state.currentTurn = currentTurn === 'X' ? 'O' : 'X';
         },
         restart: (state, action) => {
@@ -25,9 +29,35 @@ export const gameSlice = createSlice({
     },
 });
 
+const getWinner = (gridState, player, history) => {
+    for (let i = 0; i < gridState.length; i++) {
+        /**horizontal rule-checking */
+        const sameRow = gridState[i].every((item) => { return item === player });
+        if (sameRow) {
+            return player
+        }
+
+        /**vertical rule-checking */
+        let sameCol = true;
+        for (let j = 0; j < gridState.length; j++) {
+            if (gridState[j][i] !== player) {
+                sameCol = false;
+            }
+        }
+        if (sameCol) {
+            return player
+        }
+
+        /**diagonal rule-checking */
+    }
+    if (history.length === 9) {
+        return 'tie';
+    }
+    else return '';
+}
+
 export const { tileClick, restart } = gameSlice.actions;
 
-export const getStatus = state => state.game.boardStatus;
-export const getCurrent = state => state.game.currentTurn;
+export const getBoardStatus = state => state.game;
 
 export default gameSlice.reducer;
